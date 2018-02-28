0ab0f152d399b0bef6ad26ce040366d537c4a1ab
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 02:39:57 +0900

    improving

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
           var month = now.getMonth() + 1;
           var day = now.getDate();
           var year = now.getFullYear();
-          var hours = (now.getHours() + 9)%24;
+          var hours = now.getHours();
           var minutes = now.getMinutes();
           var seconds = now.getSeconds();
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
