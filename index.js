ea882a23744079d293769cf2ccacc358887a1e64
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 02:06:42 +0900

    improving

--- index.js
+++ index.js
@@ -31,49 +31,17 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "会津 太郎"){
+            //if (event.message.text == "会津 太郎"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                setName(event.message.text);
-                name = getName();
+                var name = event.message.text;
                 events_processed.push(bot.replyMessage(event.replyToken, {
                   type: "text",
                   text: name
                 }));
-            } else if(event.message.text == "s1240236"){
-                setId(event.message.text);
-                id = getId();
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                  type: "text",
-                  text: id
-                }));
-            } else if(event.message.text == "確認"){
-                id = getId();
-                name = getName();
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                  type: "text",
-                  text: id + ", " + name
-                }));
-            }
+            //}
         }
     });
 
-    var id = '';
-    var name = '';
-    function setId(data){
-      id = data;
-    }
-
-    function setName(data){
-      name = data;
-    }
-
-    function getId(){
-      return id;
-    }
-
-    function getName(){
-      return name;
-    }
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
     Promise.all(events_processed).then(
         (response) => {
