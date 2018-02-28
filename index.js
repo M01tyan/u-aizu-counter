d8dcb2f6bfff6dddc9c6c170f44125d66366ef74
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 01:19:47 +0900

    improving

--- index.js
+++ index.js
@@ -27,13 +27,14 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
     let id = '';
-    let name = '';
     // イベントオブジェクトを順次処理。
     req.body.events.map((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            id = event.message.text;
+            if (event.messages.text != "こんにちは"){
+              id = event.message.text;
+            }
             if (event.message.text == "こんにちは"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
