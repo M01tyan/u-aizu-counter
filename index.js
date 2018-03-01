6683883cdbe765843a2f21a94ee2c0f5badd6323
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 22:00:41 +0900

    improving

--- index.js
+++ index.js
@@ -125,28 +125,71 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               //mode = "absence";
               events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "template",
-                altText: "",
-                template: {
-                  type: "image_carousel",
-                  columns: [
-                    {
-                      imageUrl: count5,
-                      action: {
-                        type: "postback",
-                        label: "Buy",
-                        data: "action=buy&itemid=111"
-                      }
-                    },
-                    {
-                      imageUrl: "https://page.line.me/cjg2466f/count_number/count4.jpg",
-                      action: {
-                        type: "postback",
-                        label: "Buy",
-                        data: "action=buy&itemid=111"
-                      }
-                    }
-                  ]
+  altText: "this is a carousel template",
+  template: {
+      type: "carousel",
+      columns: [
+          {
+            thumbnailImageUrl: "https://example.com/bot/images/item1.jpg",
+            imageBackgroundColor: "#FFFFFF",
+            title: "this is menu",
+            text: "description",
+            defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://example.com/page/123"
+            },
+            actions: [
+                {
+                    type: "postback",
+                    label: "Buy",
+                    data: "action=buy&itemid=111"
+                },
+                {
+                    type: "postback",
+                    label: "Add to cart",
+                    data: "action=add&itemid=111"
+                },
+                {
+                    type: "uri",
+                    label: "View detail",
+                    uri: "http://example.com/page/111"
                 }
+            ]
+          },
+          {
+            thumbnailImageUrl: "https://example.com/bot/images/item2.jpg",
+            imageBackgroundColor: "#000000",
+            title: "this is menu",
+            text: "description",
+            defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://example.com/page/222"
+            },
+            actions: [
+                {
+                    type: "postback",
+                    label: "Buy",
+                    data: "action=buy&itemid=222"
+                },
+                {
+                    type: "postback",
+                    label: "Add to cart",
+                    data: "action=add&itemid=222"
+                },
+                {
+                    type: "uri",
+                    label: "View detail",
+                    uri: "http://example.com/page/222"
+                }
+            ]
+          }
+      ],
+      imageAspectRatio: "rectangle",
+      imageSize: "cover"
+  }
+}
               }));
             }
           }
