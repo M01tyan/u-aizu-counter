14f97c7c3af44a33fb81bd5209e9bc519c24841c
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 21:53:25 +0900

    improving

--- index.js
+++ index.js
@@ -124,10 +124,6 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
             } else if(event.message.text == "欠席数カウント"){
               //mode = "absence";
               events_processed.push(bot.replyMessage(event.replyToken, {
-                type: "text",
-                text: "ok"
-              }));
-              events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "template",
                 altText: "",
                 template: {
