f5bec4119f5cbf908814646db7f84fbce0eb211a
M01tyan <kanta01m.tyan@gmail.com>
2018-03-01 14:15:00 +0900

    improving

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               count += 1;
               events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "text",
-                text: userId + ", " + UserName + ", " + userDivision
+                text: userId + ", " + userName + ", " + userDivision
               }));
               //クラスがきちんと入力されていない場合はもう一度
             } else {
