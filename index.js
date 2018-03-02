03e8d082d04775319775b1a32736e11832608316
M01tyan <kanta01m.tyan@gmail.com>
2018-03-02 16:06:40 +0900

    improving

--- index.js
+++ index.js
@@ -46,10 +46,18 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               if(id[3] == 6 || id[3] == 5){
                 mode = "divisionInit";
                 events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "あなたのクラスを入力してください。"
-                },{type: "text",
-                text: "a"}));
+                    type: "template",
+                    template: [
+                      {
+                        type: "message",
+                        text: "あなたのクラスを入力してください。"
+                      },
+                      {
+                        type: "text",
+                        text: "a"
+                      }
+                    ]
+                }));
                 //3,4,5年生はフィールドを入力
               } else if(id[3] == 4 || id[3] == 3 || id[3] == 2){
                 mode = "divisionInit";
