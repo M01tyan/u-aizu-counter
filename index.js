393ad075c19f515fb72700d27036ea7a7f244379
M01tyan <kanta01m.tyan@gmail.com>
2018-03-03 23:05:50 +0900

    improving

--- index.js
+++ index.js
@@ -278,10 +278,13 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               //var save = absence_count.template.columns.pop();
               absence_count.template.columns.push(class_count);
               //absence_count.template.columns.push(save);
-              events_processed.push(bot.replyMessage(event.replyToken, {
+              events_processed.push(bot.replyMessage(event.replyToken, [{
                 type: "text",
                 text: event.message.text + "を追加しました。"
-              }));
+              },{
+                type: "text",
+                text: absence_count.template.columns
+              }]));
             }
           }
         }
