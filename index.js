49b7ed0a0d8aba6180d13657265911fdc35f96f1
M01tyan <kanta01m.tyan@gmail.com>
2018-03-03 22:02:28 +0900

    improving

--- index.js
+++ index.js
@@ -311,10 +311,16 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
             lesson.push({"name": event.message.text, "count": 3});
             absence_count.template.columns[i].title = lesson[i].name;
             absence_count.template.columns[i].thumbnailImageUrl += (lesson[i].count + ".jpg");
-            events_processed.push(bot.replyMessage(event.replyToken, {
-              type: "text",
-              text: event.message.text + "を追加しました。"
-            }));
+            events_processed.push(bot.replyMessage(event.replyToken, [
+              {
+                type: "text",
+                text: event.message.text + "を追加しました。"
+              },
+              {
+                type: "text",
+                text: absence_count.template.columns[i].thumbnailImageUrl
+              }
+            ]));
             i += 1;
           }
         }
