b9bec4f4b7ab46e1ffcb9582a146ff626ac3143f
M01tyan <kanta01m.tyan@gmail.com>
2018-03-03 23:03:43 +0900

    improving

--- index.js
+++ index.js
@@ -275,9 +275,9 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               }));
             } else {
               class_count.title = event.message.text;
-              var save = absence_count.template.columns.pop();
+              //var save = absence_count.template.columns.pop();
               absence_count.template.columns.push(class_count);
-              absence_count.template.columns.push(save);
+              //absence_count.template.columns.push(save);
               events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "text",
                 text: event.message.text + "を追加しました。"
