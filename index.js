24a57b95e89fb7e7ba85eda9d1d82ab4f72e422d
M01tyan <kanta01m.tyan@gmail.com>
2018-03-05 16:57:16 +0900

    improving

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                     "type": "buttons",
                     "imageSize": "cover",
                     "imageBackgroundColor": "#FFFFFF",
-                    "text": "授業を追加する学期を選んでください。",
+                    "title": "授業を追加する学期を選んでください。",
                     "actions": [
                         {
                           "type": "message",
