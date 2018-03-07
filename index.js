72d82a70efd0d249eca0f3da02ba5a2ed9ffa6c3
M01tyan <kanta01m.tyan@gmail.com>
2018-03-07 15:50:10 +0900

    improving

--- index.js
+++ index.js
@@ -383,19 +383,19 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               };
               if(userGrade == "3年"){
                 if(userDivision == "IT-SPR"){
-                  for(let j=0; j<spr_third[semester].length; j+=1){
-                    if(event.message.text == spr_third[semester][j].name){
-                      class_count.title = spr_third[semester][j].code + " " + spr_third[semester][j].name;
-                      class_count.text = spr_third[semester][j].table + " " + spr_third[semester][j].time + "\n" + spr_third[semester][j].room + " " + spr_third[semester][j].instructor + "\n単位数：" + spr_third[semester][j].credits;
+                  for(let i=0; i<spr_third[semester].length; i+=1){
+                    if(event.message.text == spr_third[semester][i].name){
+                      class_count.title = spr_third[semester][i].code + " " + spr_third[semester][i].name;
+                      class_count.text = spr_third[semester][i].table + " " + spr_third[semester][i].time + "\n" + spr_third[semester][i].room + " " + spr_third[semester][i].instructor + "\n単位数：" + spr_third[semester][i].credits;
                       absence_count.template.columns.push(class_count);
                       events_processed.push(bot.replyMessage(event.replyToken, {
                         type: "text",
-                        text: spr_third[semester][j].code + " " + spr_third[semester][j].name + "を追加しました。"
+                        text: spr_third[semester][i].code + " " + spr_third[semester][i].name + "を追加しました。"
                       }));
                       break;
                     }
                   }
-                  if(j == spr_third[semester].length) {
+                  if(i == spr_third[semester].length) {
                     events_processed.push(bot.replyMessage(event.replyToken, {
                       type: "text",
                       text: "授業がありません\nもう一度入力してください"
