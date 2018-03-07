2b0e8ae2cb89c5580ddf5d068e6fa41ce1286f50
M01tyan <kanta01m.tyan@gmail.com>
2018-03-07 15:23:56 +0900

    improving

--- index.js
+++ index.js
@@ -339,7 +339,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                   "MA01"
                 },{
                   type: "text",
-                  text: "授業追加を終了するときは\n終了と入力してください。\n"
+                  text: "授業追加を終了するときは\n終了と入力してください。" + spr_third[0][spr_third[0].length-1].name
                 }
               ]));
             } else if(event.message.text === "終了"){
@@ -383,16 +383,8 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
               };
               if(userGrade == "3年"){
                 if(userDivision == "IT-SPR"){
-                  for(var j=0; j<spr_third[semester].length; j+=1){
+                  for(let j=0; j<spr_third[semester].length; j+=1){
                     if(event.message.text.match == spr_third[semester][j].name){
-                      class_count.title = spr_third[semester][j].code + " " + spr_third[semester][j].name;
-                      class_count.text = spr_third[semester][j].table + " " + spr_third[semester][j].time + "\n" + spr_third[semester][j].room + " " + spr_third[semester][j].instructor + "\n単位数：" + spr_third[semester][j].credits;
-                      absence_count.template.columns.push(class_count);
-                      events_processed.push(bot.replyMessage(event.replyToken, {
-                        type: "text",
-                        text: spr_third[semester][j].code + " " + spr_third[semester][j].name + "を追加しました。"
-                      }));
-                    } else if(event.message.text == spr_third[semester][j].code){
                       class_count.title = spr_third[semester][j].code + " " + spr_third[semester][j].name;
                       class_count.text = spr_third[semester][j].table + " " + spr_third[semester][j].time + "\n" + spr_third[semester][j].room + " " + spr_third[semester][j].instructor + "\n単位数：" + spr_third[semester][j].credits;
                       absence_count.template.columns.push(class_count);
